1317513ec9c0eadba9805e939e10b16b09ed60f3
Jorge Diniz <onediniz2@gmail.com>
2021-12-03 01:57:01 -0300

    Primeiro Commit

---
+++ LaBrasa/Shared/Contracts/IProdutoRepository.cs
@@ -0,0 +1,15 @@
+﻿using LaBrasa.Shared.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LaBrasa.Shared.Contracts
+{
+    public interface IProdutoRepository
+    {
+        Task<Produto> AddAsync(Produto produto);
+        Task<List<Produto>> GetAllAsync();
+        Task<Produto> GetById(int id);
+        Task<Produto> UpdateAsync(Produto produto);
+        Task Delete(int id);
+    }
+}
---
+++ LaBrasa/Shared/Repository/ProdutosRepository.cs
@@ -0,0 +1,41 @@
+﻿using LaBrasa.Shared.Contracts;
+using LaBrasa.Shared.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using con = LaBrasa.Shared.Context;
+
+namespace LaBrasa.Shared.Repository
+{
+    public class ProdutosRepository : IProdutoRepository
+    {
+        private readonly con.Context _context;
+        public ProdutosRepository(con.Context context)
+        {
+            _context = context;
+        }
+        public Task<Produto> AddAsync(Produto produto)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Delete(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<Produto>> GetAllAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Produto> GetById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Produto> UpdateAsync(Produto produto)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
